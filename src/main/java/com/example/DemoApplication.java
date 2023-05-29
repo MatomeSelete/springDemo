84b49c4680f0a186123d5d0fdd4bc95fc636dbcc
Matome <seletematome3@gmail.com>
2023-05-29 08:39:28 +0200

    api is working

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,15 +1,35 @@
 package com.example;
 
+import com.example.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Matome",
+						"matome@gmail.com",
+						LocalDate.of(1996, 11, 26),
+						25
+
+				)
+		);
 	}
 
 }
